2e1f9b35b5ca4bd8515b3a66f5499c8aff46731b
ekpei <anthony4arsenal@gmail.com>
2023-08-28 15:49:09 +0100

    Initial commit

---
+++ app/src/main/java/cub/sys360/shimmerapplication/MainActivity.kt
@@ -0,0 +1,51 @@
+package cub.sys360.shimmerapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cub.sys360.shimmerapplication.ui.theme.ShimmerApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShimmerApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                  Column() {
+                      repeat(5){
+                          shimmerLoading()
+                      }
+                  }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShimmerApplicationTheme {
+        Greeting("Android")
+    }
+}
